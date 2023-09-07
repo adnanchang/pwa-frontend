43a19eaa0c84dc6a75b44487d42741992d716cd4
Muhammad Adnan <ma@wobe-systems.com>
2023-09-07 09:22:10 +0200

    Hehe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,6 +16,8 @@
       <div class="row">
         ONCLOSE: {{ onClose }}
       </div> -->
+
+      <h3>AFTER ASKING HELLO {{ str }}</h3>
     </div>
   </div>
 </template>
@@ -53,9 +55,10 @@ import axios from 'axios'
 //   console.log(ev)
 //   onClose.value = JSON.stringify(ev)
 // }
+const str = ref('')
 onMounted(async () => {
   try {
-    const res = await axios.get('https://localhost:8443/')
+    const res = await axios.get('https://imaginative-moonbeam-04f17a.netlify.app/api/hello')
 
     console.log('from comp', res.data)
   } catch (error) {
