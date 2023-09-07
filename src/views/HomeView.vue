7d947a86f6c2cb7a651c3b1830d56f9f0371a08c
Muhammad Adnan <ma@wobe-systems.com>
2023-09-07 15:48:28 +0200

    hacky shacky

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,9 @@
       <q-btn @click="requestNotificationPermission()"> allow </q-btn>
 
       <h4>permission {{ permission }}</h4>
-      <h6>service worker {{ regitration }}</h6>
+      <h6>err {{ JSON.parse(error) }}</h6>
+      <h6>sub {{ JSON.parse(sub) }}</h6>
+      <h6>res {{ JSON.parse(res) }}</h6>
     </div>
   </div>
 </template>
@@ -19,15 +21,25 @@ import axios from 'axios'
 const str = ref('')
 const permission = ref('')
 const regitration = ref<ServiceWorkerRegistration | ''>('')
+
+
+const error = ref('');
+const sub = ref('');
+const res = ref('')
+
 onMounted(async () => {
   try {
-    const res = await axios.get('https://imaginative-moonbeam-04f17a.netlify.app/api/hello')
+    const resp = await axios.get('https://imaginative-moonbeam-04f17a.netlify.app/api/hello')
 
-    console.log('from comp', res.data)
+    console.log('from comp', resp.data)
 
-    str.value = res.data
+    str.value = resp.data
 
     permission.value = window.Notification.permission
+    
+    error.value = localStorage.getItem('error') || '';
+    sub.value = localStorage.getItem('sub') || '';
+    res.value = localStorage.getItem('res') || '';
   } catch (error) {
     console.error(error)
   }
